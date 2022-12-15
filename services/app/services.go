42a2b9e876a394a96ef183149c92b9e85d053d5a
Hiro <laciferin@gmail.com>
2022-12-16 04:33:42 +0530

    :sparkles: url
    Signed-off-by: Hiro <laciferin@gmail.com>

--- services/app/services.go
+++ services/app/services.go
@@ -1,4 +1,52 @@
 package app
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Services interface {
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// randStringBytes - Create random short link
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+var urlMap = make(map[string]string)
+
+var revUrlMap = make(map[string]string)
+
+func (s *service) ShortenUrl(url string) (shortenedUrl string) {
+
+	shortenedUrl = urlMap[url]
+
+	if shortenedUrl == "" {
+	SHORT_URL:
+		shortenedUrl = randStringBytes(10)
+
+		if revUrlMap[shortenedUrl] != "" {
+			goto SHORT_URL
+		}
+
+		revUrlMap[shortenedUrl] = url
+
+	}
+	return
+}
+
+func (s *service) RetrieveOriginalUrl(shortUrl string) (url string, err error) {
+
+	url = revUrlMap[shortUrl]
+
+	if url == "" {
+		err = fmt.Errorf("url not found")
+	}
+	return
 }
